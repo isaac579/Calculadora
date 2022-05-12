5776ed8e7193948f8185fdba47e7bfa13f6562c1
isaac579 <isaaccalva135@gmail.com>
2022-05-12 10:36:34 -0500

    Prueba1

---
+++ JS/funcion.js
@@ -0,0 +1,5 @@
+const displayAnterior = document.getElementById("valorAnterior");
+const displayActual = document.getElementById("valorActual");
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('Operador');
+
